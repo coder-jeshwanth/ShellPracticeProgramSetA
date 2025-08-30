1445560ba9776700478025390fcf2c234eae0fac
coder-jeshwanth <24mcaa32@kristujayanti.com>
2025-08-30 03:11:27 +0000

    Second commit

--- cal.sh
+++ cal.sh
@@ -9,7 +9,8 @@ echo "Choose operation:"
 echo "1. Addition"
 echo "2. Subtraction"
 echo "3. Multiplication"
-read -p "Enter choice [1-3]: " choice
+echo "4. Division"
+read -p "Enter choice [1-4]: " choice
 
 # Perform the selected operation
 case $choice in
@@ -25,7 +26,15 @@ case $choice in
     result=$((num1 * num2))
     echo "Result: $num1 * $num2 = $result"
     ;;
+  4)
+    if [ "$num2" -eq 0 ]; then
+      echo "Error: Division by zero is not allowed."
+    else
+      result=$(echo "scale=2; $num1 / $num2" | bc)
+      echo "Result: $num1 / $num2 = $result"
+    fi
+    ;;
   *)
-    echo "Invalid choice. Please select 1, 2, or 3."
+    echo "Invalid choice. Please select 1, 2, 3, or 4."
     ;;
 esac
--- sal.sh
+++ sal.sh
@@ -6,6 +6,9 @@ read -p "Enter your salary: " salary
 # Calculate tax based on salary brackets
 if [ "$salary" -lt 20000 ]; then
     tax=0
+elif [ "$salary" -eq 20000 ]; then
+    tax=$(echo "$salary * 0.02" | bc)
+
 elif [ "$salary" -le 40000 ]; then
     tax=$(echo "$salary * 0.05" | bc)
 else
